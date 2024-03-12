d74f65bccbbb89e8561c313c006e9e6cfe783926
Sachin Singh <102596511+Narc2022@users.noreply.github.com>
2024-03-12 09:22:43 +0530

    added email verification

--- routes/user.js
+++ routes/user.js
@@ -196,4 +196,72 @@ router.post("/verify/mobile", (req, res) => {
   }
 });
 
+// Email Verification
+
+const nodemailer = require("nodemailer");
+const randomstring = require("randomstring");
+const userEmail = process.env.userEmail;
+const userPass = process.env.userPass;
+const transporter = nodemailer.createTransport({
+  service: "Gmail", // Change to your email service
+  auth: {
+    user: userEmail, // Change to your email
+    pass: userPass, // Change to your password
+  },
+});
+
+// Route to generate and send OTP
+router.post("/send-otp", (req, res) => {
+  const email = req.body.email;
+
+  // Generate OTP
+  const otp = randomstring.generate({
+    length: 6,
+    charset: "numeric",
+  });
+  otpMap.set(email, otp);
+
+  // Send OTP via email
+  const mailOptions = {
+    from: "sachinnarofficials@gmail.com",
+    to: email,
+    subject: "OTP Verification",
+    text: `Your OTP is: ${otp}`,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.status(500).json({ message: "Failed to send OTP" });
+    } else {
+      console.log("Email sent: " + info.response);
+      res.json({ message: "OTP sent successfully" });
+    }
+  });
+});
+
+// Route to verify OTP
+router.post("/verify-otp", (req, res) => {
+  const { email, otp } = req.body;
+  if (!otpMap.has(email)) {
+    return res
+      .status(400)
+      .json({ success: false, message: "OTP not sent for this email" });
+  }
+
+  const storedOTP = otpMap.get(email);
+  console.log("storedOTP", storedOTP);
+  if (otp == storedOTP) {
+    otpMap.delete(email);
+    res.json({
+      success: true,
+      message: `Email ${email} verified successfully`,
+    });
+  } else {
+    res.status(400).json({ success: false, message: "Incorrect OTP" });
+  }
+  // In a real-world scenario, you would compare the OTP received with the one stored against the user
+  // For simplicity, we're just sending a success response if OTP matches "123456"
+});
+
 module.exports = router;
